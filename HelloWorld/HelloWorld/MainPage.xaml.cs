268dd321e13ce6d487b1e9fc40181a0f7f59d38c
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2022-12-28 22:14:10 +0100

    Add project files.

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void Handle_Click(object sender,EventArgs eventArgs)
+        {
+            count++;
+            var btn=(Button)sender;
+            btn.Text = $"butone {count} kere tiklandi!";
+        }
+    }
+}
